fc5f4658ec00acf367cd600a06a0d513c1103a6f
acaparro <acaparroso97@gmail.com>
2018-12-18 15:16:16 -0800

    added the genetic Algorithm demo so that i can work on it from home

--- Genetic_Algorithm_Demo.py
+++ Genetic_Algorithm_Demo.py
@@ -14,23 +14,22 @@ def main():
 	pygame.init()
 	game_width = 1920
 	game_height = 1080
-	white = [255,255,255]
 	gray = [127,127,127]
 	black = [0,0,0]
 	red = [255,0,0]
 	green = [0,255,0]
 	blue = [0,0,255]
-	fps = 60
+	fps = 100
 	numBots = 100
 	numGenerations = 10
 	numChangePerLife = 100
 	mutation_rate = 0.2
-	reproduction_rate = 0.0005
+	reproduction_rate = 0.005
 	initial_perception_radius = 100
 	boundary_size = 10
 	max_vel = 10
-	colour_change_rate = 0.03
-	colour_mutation_rate = 0.1
+	colour_change_rate = 0.00003
+	colour_mutation_rate = 0.005
 	initial_max_force = 0.02
 	bots = []
 	best_ever = 0
@@ -40,15 +39,16 @@ def main():
 
 	#Change the colour of the bot over acquired change.
 	def acquiredChange(colour_code):
+		print(colour_code)
 		# minus means moving towards black plus means moving towards white.
-		percent_change = 1+(random.random() * colour_change_rate)
+		percent_change = (random.random() * colour_change_rate)
 		#Move towards clearer colour
 		if(percent_change > 1):
-			acquiredChanged_colour = (min(max((percent_change)*colour_code[0],0),255), min(max(percent_change*colour_code[1],0),255), min(max(percent_change*colour_code[2],0),255))
-			acquiredChanged_colour_code = [min(max((percent_change)*colour_code[0],0),255)], [min(max(percent_change*colour_code[1],0),255)], [min(max(percent_change*colour_code[2],0),255)];
+			acquiredChanged_colour = (min(max(((percent_change*255) + colour_code[0]) +colour_code[0],0),255), min(max(((percent_change*255) + colour_code[0]) +colour_code[1],0),255), min(max(((percent_change*255) + colour_code[0]) +colour_code[2],0),255))
+			acquiredChanged_colour_code = [min(max(((percent_change*255) + colour_code[0]) +colour_code[0],0),255), min(max(((percent_change*255) + colour_code[0]) +colour_code[1],0),255), min(max(((percent_change*255) + colour_code[0]) +colour_code[2],0),255)]
 		elif(percent_change<1):
-			acquiredChanged_colour = (max(min((percent_change)*colour_code[0],255),0), max(min(percent_change*colour_code[1],255),0), max(min(percent_change*colour_code[2],255),0))
-			acquiredChanged_colour_code = [max(min((percent_change)*colour_code[0],255),0)], [max(min(percent_change*colour_code[1],255),0)], [max(min(percent_change*colour_code[2],255),0)];
+			acquiredChanged_colour = (max(min(((percent_change*255) + colour_code[0]) +colour_code[0],255),0), max(min(((percent_change*255) + colour_code[0]) +colour_code[1],255),0), max(min(((percent_change*255) + colour_code[0]) +colour_code[2],255),0))
+			acquiredChanged_colour_code = [max(min(((percent_change*255) + colour_code[0]) +colour_code[0],255),0), max(min(((percent_change*255) + colour_code[0]) +colour_code[1],255),0), max(min(((percent_change*255) + colour_code[0]) +colour_code[2],255),0)]
 		else:
 			acquiredChanged_colour = (colour_code[0],colour_code[1],colour_code[2])
 			acquiredChanged_colour_code = colour_code
@@ -62,14 +62,20 @@ def main():
 
 			if dna != False:
 				#array to keep track of genetics in this case, it keeps track of the colour at the start of the lifecycle of the bot.
-				self.dna = []
+				self.dna = dna
 				
 				#if new offspring triggers a mutation
 				if random.random() < mutation_rate:
 					#We move the colour slightly (non randomly)
-					self.colour_code[0] = dna[0]*colour_mutation_rate
-					self.colour_code[1] = dna[1]*colour_mutation_rate
-					self.colour_code[2] = dna[2]*colour_mutation_rate
+					mutation_amount = (colour_mutation_rate * 255)
+					if((dna[0] - mutation_amount) < 0):
+						self.colour_code[0] = 0
+						self.colour_code[1] = 0
+						self.colour_code[2] = 0
+					else:
+						self.colour_code[0] = dna[0] - (mutation_amount)
+						self.colour_code[1] = dna[1] - (mutation_amount)
+						self.colour_code[2] = dna[2] - (mutation_amount)
 					self.dna = self.colour_code
 					self.colour = (self.colour_code[0], self.colour_code[1], self.colour_code[2])
 				else:
@@ -80,27 +86,29 @@ def main():
 			else:
 				#if it is a new bot, then set the color dna to be grey.
 				self.dna = gray
-			print(self.dna)
 
 
 		def update(self):
 			self.colour, self.colour_code = acquiredChange(self.colour_code)
+			
 		def reproduce(self):
-			bots.append(create_bot(self.position[0], self.position[1], self.dna))
+			bots.append(create_bot(self.position[0]+10, self.position[1]+10, self.dna))
 
 
-		def draw_bot(self):
-			pygame.gfxdraw.aacircle(gameDisplay, int(self.position[0]), int(self.position[1]), 5, self.colour)
-			pygame.gfxdraw.filled_circle(gameDisplay, int(self.position[0]), int(self.position[1]), 5, self.colour)
+		def draw_bot(self, x, y):
+			pygame.gfxdraw.aacircle(gameDisplay, int(x*20), int(y*20), 20, self.colour)
+			pygame.gfxdraw.filled_circle(gameDisplay, int(x*20), int(y*20), 20, self.colour)
 			
 	#instantiation of bots
 	for i in range(numBots):
 		bots.append(create_bot(random.uniform(0,game_width),random.uniform(0,game_height)))
 	running = True
-	countGen = 0
+	countGen = 1
 	countChange = 0
 	while(running):
 		gameDisplay.fill(blue)
+		pygame.gfxdraw.aacircle(gameDisplay, 200, 200, 20, (127,127,127))
+		pygame.gfxdraw.filled_circle(gameDisplay, 200, 200, 20, (127,127,127))
 		#set limit of generations.
 		if(countGen >= numGenerations):
 			running = False
@@ -109,28 +117,37 @@ def main():
 				running = False
 
 		#go through acquired changes with all the bots
-		for bot in bots[::-1]:
+		count = 1
+		for bot in bots:
 			bot.update()
-			bot.draw_bot()
-
+			bot.draw_bot(count,countGen)
+			count += 1
+		
 		#set limit of acquired changes.
 		if(countChange >= numChangePerLife):
+			print("The number of bots is = ",len(bots), "The last for loop got called ", count, " times")
 			#once acquired changes run, it is time to die and reproduce
 			countGen += 1
 			countChange = 0
-			print('Generation ' + countGen + ': \n')
+			print('Generation ' , countGen , ': \n')
 			#sort the list of bots (reverse if black is objective, default if white is objective) To get the best of the generation.
-			bots.sort(reverse = True)
+			bots.sort(key=lambda t: t.colour_code[0], reverse = True)
 			#print the best values for this generation
 			best_ever = bots[0].colour_code
-			best_ever_dna = bot.dna
+			best_ever_dna = bots[0].dna
 			print(best_ever, best_ever_dna)
 			#grab the top 25% of population and reproduce them 4 times. Kill the rest.
 			generationBest = []
-			for i in range(numBots *0.25):
+			numReplic = int(numBots *0.25)
+			print("This is how many bots after the call: ",numReplic)
+			for i in range(numReplic):
 				generationBest.append(bots[i])
+			
 			#new generation
-			bots[:] = []
+			print("Length of bots before deletion: ", len(bots))
+			del bots[:]
+			print("Length of bots after deletion: ", len(bots))
+			print("length of generationBest ", len(generationBest))
 			for i in range(len(generationBest)):
 				for j in range(4):
 					generationBest[j].reproduce()
